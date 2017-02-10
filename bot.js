f45c4f64a4daec29e97bbc59a5e8e32a3976638f
Kasey Schlaudt <kaseyschlaudt@gmail.com>
2017-02-10 09:56:19 -0700

    made the bot reply to a user

--- bot.js
+++ bot.js
@@ -2,20 +2,26 @@ var Twitter = require('twitter');
 var credentials = require('./credentials');
 var client = new Twitter(credentials);
 
+var statuses = ['awsome', 'cool', 'keep up the awsome content']; //make sure you have many more responses
+var replyCurrentNum = 0
+
 function favoriteUsersTweets() {
     client.get('statuses/user_timeline', {
         screen_name: "the_kaseys"
     }, function(e, r) {
+        console.log('we here');
         for (var i = 0; i < r.length; i++) {
-            client.post('favorites/create/', {
-                id: r[i].id_str
+            client.post('statuses/update', {
+                status: statuses[replyCurrentNum],
+                in_reply_to_status_id: r[i].id_str
             }, function(e, r) {
+                replyCurrentNum += 1;
                 console.log(r);
             });
         }
     });
 }
-
+favoriteUsersTweets();
 setInterval(function() {
     favoriteUsersTweets();
 }, 5 * 60 * 60000); //every five hours
